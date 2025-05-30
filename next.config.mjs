7a0a0735e3b9877987b902f9437480e501843f87
vigneshtd <vigneshtd111@gmail.com>
2025-05-30 22:57:38 +0530

    eslint ignores, removed empty routes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-
-  /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
+  
+  
   const nextConfig = {
-    
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
   };
   export default nextConfig;
