db98de9f8ab9155be1164d53d617af7342f36384
vigneshtd <vigneshtd111@gmail.com>
2025-06-01 22:23:51 +0530

    added ! for secret declaration

--- lib/auth.ts
+++ lib/auth.ts
@@ -22,7 +22,7 @@ export const authOptions: NextAuthOptions = {
       
         }),
     ],
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET!,
     session:{
         strategy:"jwt",
 
