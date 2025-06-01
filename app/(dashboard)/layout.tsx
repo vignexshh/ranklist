ff0009bfde61001aa33e887bf2a217281294a1d3
vigneshtd <vigneshtd111@gmail.com>
2025-06-01 21:59:01 +0530

    binary changes at prisma schema

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
-import { Fullscreen } from '@mui/icons-material';
+
 
 export default function Layout(props: { children: React.ReactNode }) {
   return (
