6e73e5aaa96ff7463c6aab4b4ca8f3bebf19a38c
vigneshtd <vigneshtd111@gmail.com>
2025-06-01 22:37:06 +0530

    logs for debugging envs

--- lib/auth.ts
+++ lib/auth.ts
@@ -14,6 +14,7 @@ declare module "next-auth" {
 }
 
 export const authOptions: NextAuthOptions = {
+    
 
     providers: [
         GoogleProvider({
@@ -23,6 +24,7 @@ export const authOptions: NextAuthOptions = {
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET!,
+    
     session:{
         strategy:"jwt",
 
